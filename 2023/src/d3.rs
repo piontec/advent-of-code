3d78e49b73ebe047d2e226e9356ef3db4ade615b
Łukasz Piątkowski <piontec@gmail.com>
2023-12-14 23:31:48 +0100

    refactoring for d3

--- 2023/src/d3.rs
+++ 2023/src/d3.rs
@@ -2,7 +2,7 @@ use crate::DayTask;
 
 pub struct Task;
 
-impl DayTask<i32> for Task {
+impl DayTask<usize> for Task {
     fn day_no(&self) -> u8 {
         3
     }
@@ -24,61 +24,71 @@ impl DayTask<i32> for Task {
         todo!()
     }
 
-    fn get_part1_test_result(&self) -> i32 {
+    fn get_part1_test_result(&self) -> usize {
         4361
     }
 
-    fn get_part2_test_result(&self) -> i32 {
+    fn get_part2_test_result(&self) -> usize {
         todo!()
     }
 
-    fn run_p1(&self, lines: Vec<String>) -> i32 {
+    fn run_p1(&self, lines: Vec<String>) -> usize {
+        // answer: 537732
         let mut result = 0;
-        for (li, line) in lines.iter().enumerate() {
-            let mut digit_start_index: Option<usize> = Option::None;
-            let mut digit_end_index = 0;
-            for (i, c) in line.chars().enumerate() {
-                if c.is_digit(10) && digit_start_index.is_none() {
-                    digit_start_index = Option::Some(i);
-                    continue;
-                }
-                if c.is_digit(10) && digit_start_index.is_some() {
-                    continue;
-                }
-                if !c.is_digit(10) && digit_start_index.is_some() {
-                    digit_end_index = i - 1;
-
-                    let (num, has_symbol) = get_num(
-                        li,
-                        &lines,
-                        digit_start_index.unwrap(),
-                        digit_end_index,
-                    );
-                    if has_symbol {
-                        result += num;
-                    }
-                    digit_start_index = Option::None;
-                }
+        for numpos in get_numpos(&lines) {
+            if numpos.symbol.is_none() {
+                continue;
             }
-            if digit_start_index.is_some() {
-                digit_end_index = line.len() - 1;
-                let (num, has_symbol) = get_num(
-                    li,
-                    &lines,
-                    digit_start_index.unwrap(),
-                    digit_end_index,
-                );
-                if has_symbol {
-                    result += num;
-                }
-            }
+            result += numpos.num;
         }
         result
     }
 
-    fn run_p2(&self, lines: Vec<String>) -> i32 {
+    fn run_p2(&self, lines: Vec<String>) -> usize {
         todo!()
     }
+}
+
+struct Position {
+    line_index: usize,
+    char_index: usize,
+}
+
+struct SymbolNum {
+    position: Position,
+    symbol: Option<char>,
+    num: usize,
+}
+
+fn get_numpos(lines: &Vec<String>) -> Vec<SymbolNum> {
+    let mut result: Vec<SymbolNum> = Vec::new();
+
+    for (li, line) in lines.iter().enumerate() {
+        let mut digit_start_index: Option<usize> = Option::None;
+        let mut digit_end_index = 0;
+        for (i, c) in line.chars().enumerate() {
+            if c.is_digit(10) && digit_start_index.is_none() {
+                digit_start_index = Option::Some(i);
+                continue;
+            }
+            if c.is_digit(10) && digit_start_index.is_some() {
+                continue;
+            }
+            if !c.is_digit(10) && digit_start_index.is_some() {
+                digit_end_index = i - 1;
+
+                let s = get_num(li, &lines, digit_start_index.unwrap(), digit_end_index);
+                result.push(s);
+                digit_start_index = Option::None;
+            }
+        }
+        if digit_start_index.is_some() {
+            digit_end_index = line.len() - 1;
+            let s = get_num(li, &lines, digit_start_index.unwrap(), digit_end_index);
+            result.push(s);
+        }
+    }
+    result
 }
 
 fn get_num(
@@ -86,7 +96,7 @@ fn get_num(
     lines: &Vec<String>,
     digit_start_index: usize,
     digit_end_index: usize,
-) -> (i32, bool) {
+) -> SymbolNum {
     let line = &lines[line_index];
     let mut lines_to_check_indexes: Vec<usize> = Vec::new();
     if line_index > 0 {
@@ -107,6 +117,12 @@ fn get_num(
     };
 
     let mut has_symbol = false;
+    let mut symbol_pos = Position {
+        line_index: 0,
+        char_index: 0,
+    };
+    let mut symbol: Option<char> = Option::None;
+
     'of: for line_index in lines_to_check_indexes {
         for char_index in min_i..=max_i {
             let cur = lines[line_index].chars().nth(char_index).unwrap();
@@ -114,17 +130,28 @@ fn get_num(
                 continue;
             }
             has_symbol = true;
+            symbol_pos.char_index = char_index;
+            symbol_pos.line_index = line_index;
+            symbol = Option::Some(cur);
             break 'of;
         }
     }
     if !has_symbol {
         if min_i < digit_start_index && line.chars().nth(min_i).unwrap() != '.' {
-            has_symbol = true;
+            symbol_pos.char_index = min_i;
+            symbol_pos.line_index = line_index;
+            symbol = Option::Some(line.chars().nth(min_i).unwrap());
         }
         if max_i > digit_end_index && line.chars().nth(max_i).unwrap() != '.' {
-            has_symbol = true;
+            symbol_pos.char_index = max_i;
+            symbol_pos.line_index = line_index;
+            symbol = Option::Some(line.chars().nth(max_i).unwrap());
         }
     }
-    let num = str::parse::<i32>(&line[digit_start_index..=digit_end_index]).unwrap();
-    (num, has_symbol)
+    let num = str::parse::<usize>(&line[digit_start_index..=digit_end_index]).unwrap();
+    SymbolNum {
+        position: symbol_pos,
+        symbol,
+        num,
+    }
 }
