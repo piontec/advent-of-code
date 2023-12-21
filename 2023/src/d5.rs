037317f92afd928ead325ecc56aff6903219479f
Łukasz Piątkowski <piontec@gmail.com>
2023-12-21 21:55:12 +0100

    d5-2 WIP

--- 2023/src/d5.rs
+++ 2023/src/d5.rs
@@ -60,49 +60,77 @@ impl DayTask<i64> for Task {
     }
 
     fn run_p1(&self, lines: Vec<String>) -> i64 {
-        let seeds = lines[0].split(":").last().unwrap().trim().split(" ").map(|s| s.parse::<u64>().unwrap()).collect::<Vec<u64>>();
-        let mut maps = HashMap::new();
-        let mut header = None;
-        for line in lines[1..].iter() {
-            if line.is_empty() {
-                header = None;
-                continue;
-            }
-            if header.is_none() {
-                header = Some(line.split(" ").next().unwrap().trim());
-                continue;
-            }
-            if !maps.contains_key(header.unwrap()) {
-                maps.insert(header.unwrap(), SingleMap::new());
-            }
-
-            let nums = line.split(" ").map(|s| s.parse::<u64>().unwrap()).collect::<Vec<u64>>();
-            let range = Range { min: nums[1], max: nums[1] + nums[2] - 1 };
-            maps.get_mut(header.unwrap()).unwrap().insert(range, nums[0] as i64 - nums[1] as i64);
-        }
-
-        let mut locations: Vec<u64> = Vec::new();
-        for seed in seeds {
-            let mut current_value = seed;
-            'step: for step in vec!["seed-to-soil", "soil-to-fertilizer", "fertilizer-to-water", "water-to-light", "light-to-temperature", "temperature-to-humidity", "humidity-to-location"] {
-                let map = maps.get(step).unwrap();
-                for (range, offset) in map.iter() {
-                    if current_value >= range.min && current_value <= range.max {
-                        current_value = (current_value as i64 + offset) as u64;
-                        continue 'step;
-                    }
-                }
-            }
-            locations.push(current_value)
-        }
-
-        locations.iter().min().unwrap().clone() as i64
+        let maps = parse_maps(&lines[1..]);
+        
+        lines[0]
+            .split(":")
+            .last()
+            .unwrap()
+            .trim()
+            .split(" ")
+            .map(|s| find_loc(s.parse::<u64>().unwrap(), &maps))
+            .min()
+            .unwrap() as i64
     }
 
     fn run_p2(&self, lines: Vec<String>) -> i64 {
-        todo!()
+        let maps = parse_maps(&lines[1..]);
+        let seeds = lines[0]
+            .split(":")
+            .last()
+            .unwrap()
+            .trim()
+            .split(" ")
+            .map(|s| s.parse::<u64>()
+            .unwrap())
+            .collect::<Vec<u64>>()
+            .chunks(2)
+            .map(|c| (c[0]..(c[0] + c[1])).collect::<Vec<u64>>())
+            .flatten()
+            .map(|seed| find_loc(seed, &maps))
+            .min()
+            .unwrap() as i64;
+        seeds
     }
 
+}
+
+fn find_loc(seed: u64, maps: &HashMap<&str, HashMap<Range, i64>>) -> u64 {
+    let mut current_value = seed;
+    'step: for step in vec!["seed-to-soil", "soil-to-fertilizer", "fertilizer-to-water", "water-to-light", "light-to-temperature", "temperature-to-humidity", "humidity-to-location"] {
+        let map = maps.get(step).unwrap();
+        for (range, offset) in map.iter() {
+            if current_value >= range.min && current_value <= range.max {
+                current_value = (current_value as i64 + offset) as u64;
+                continue 'step;
+            }
+        }
+    }
+    current_value
+}
+
+fn parse_maps(lines: &[String]) ->  HashMap<&str, SingleMap> {
+    let mut maps = HashMap::new();
+    let mut header = None;
+    for line in lines {
+        if line.is_empty() {
+            header = None;
+            continue;
+        }
+        if header.is_none() {
+            header = Some(line.split(" ").next().unwrap().trim());
+            continue;
+        }
+        if !maps.contains_key(header.unwrap()) {
+            maps.insert(header.unwrap(), SingleMap::new());
+        }
+
+        let nums = line.split(" ").map(|s| s.parse::<u64>().unwrap()).collect::<Vec<u64>>();
+        let range = Range { min: nums[1], max: nums[1] + nums[2] - 1 };
+        maps.get_mut(header.unwrap()).unwrap().insert(range, nums[0] as i64 - nums[1] as i64);
+    }
+
+    maps
 }
 
 #[derive(Eq, Hash, PartialEq)]
