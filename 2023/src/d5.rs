663b11fa6bfc1af2ba7e7bfaf00ce45ebc2000c7
Łukasz Piątkowski <piontec@gmail.com>
2023-12-24 11:20:14 +0100

    d5-2 wip

--- 2023/src/d5.rs
+++ 2023/src/d5.rs
@@ -38,7 +38,6 @@ humidity-to-location map:
 56 93 4";
 
 impl DayTask<i64> for Task {
-
     fn day_no(&self) -> u8 {
         5
     }
@@ -61,7 +60,7 @@ impl DayTask<i64> for Task {
 
     fn run_p1(&self, lines: Vec<String>) -> i64 {
         let maps = parse_maps(&lines[1..]);
-        
+
         lines[0]
             .split(":")
             .last()
@@ -81,23 +80,103 @@ impl DayTask<i64> for Task {
             .unwrap()
             .trim()
             .split(" ")
-            .map(|s| s.parse::<u64>()
-            .unwrap())
+            .map(|s| s.parse::<u64>().unwrap())
             .collect::<Vec<u64>>()
             .chunks(2)
-            .map(|c| (c[0]..(c[0] + c[1])).collect::<Vec<u64>>())
-            .flatten()
-            .map(|seed| find_loc(seed, &maps))
+            .map(|c| Range {
+                min: c[0],
+                max: c[0] + c[1],
+            })
+            .map(|range| find_loc_range(range, &maps))
             .min()
             .unwrap() as i64;
         seeds
     }
+}
 
+fn find_loc_range(orig_range: Range, maps: &HashMap<&str, HashMap<Range, i64>>) -> u64 {
+    let mut ranges_to_check = vec![orig_range];
+    for step in vec![
+        "seed-to-soil",
+        "soil-to-fertilizer",
+        "fertilizer-to-water",
+        "water-to-light",
+        "light-to-temperature",
+        "temperature-to-humidity",
+        "humidity-to-location",
+    ] {
+        let map = maps.get(step).unwrap();
+        let mut new_ranges = vec![];
+        while ! ranges_to_check.is_empty() {
+            let range = ranges_to_check.pop().unwrap();
+            for (map_range, offset) in map.iter() {
+                // we have to check 5 cases:
+                // - range entirely within map_range,
+                if range.min >= map_range.min && range.max <= map_range.max {
+                    new_ranges.push(Range {
+                        min: (range.min as i64 + offset) as u64,
+                        max: (range.max as i64 + offset) as u64,
+                    });
+                }
+                // - map_range entirely within range,
+                else if range.min < map_range.min && range.max > map_range.max {
+                    ranges_to_check.push(Range {
+                        min: range.min,
+                        max: map_range.min,
+                    });
+                    new_ranges.push(Range {
+                        min: (map_range.min as i64 + offset) as u64,
+                        max: (map_range.max as i64 + offset) as u64,
+                    });
+                    ranges_to_check.push(Range {
+                        min: map_range.max,
+                        max: range.max,
+                    });
+                }
+                // - right side of range overlaps with map_range,
+                else if range.min < map_range.min && range.max > map_range.min {
+                    ranges_to_check.push(Range {
+                        min: range.min,
+                        max: map_range.min,
+                    });
+                    new_ranges.push(Range {
+                        min: (map_range.min as i64 + offset) as u64,
+                        max: (range.max as i64 + offset) as u64,
+                    });
+                }
+                // - left side of range overlaps with map_range,
+                else if range.min < map_range.max && range.max > map_range.max {
+                    new_ranges.push(Range {
+                        min: (range.min as i64 + offset) as u64,
+                        max: (map_range.max as i64 + offset) as u64,
+                    });
+                    ranges_to_check.push(Range {
+                        min: map_range.max,
+                        max: range.max,
+                    });
+                }
+            }
+            // range is outside of map_range
+            if new_ranges.is_empty() {
+                new_ranges.push(Range { ..range });
+            }
+        }
+        ranges_to_check = new_ranges;
+    }
+    ranges_to_check.iter().map(|r| r.min).min().unwrap()
 }
 
 fn find_loc(seed: u64, maps: &HashMap<&str, HashMap<Range, i64>>) -> u64 {
     let mut current_value = seed;
-    'step: for step in vec!["seed-to-soil", "soil-to-fertilizer", "fertilizer-to-water", "water-to-light", "light-to-temperature", "temperature-to-humidity", "humidity-to-location"] {
+    'step: for step in vec![
+        "seed-to-soil",
+        "soil-to-fertilizer",
+        "fertilizer-to-water",
+        "water-to-light",
+        "light-to-temperature",
+        "temperature-to-humidity",
+        "humidity-to-location",
+    ] {
         let map = maps.get(step).unwrap();
         for (range, offset) in map.iter() {
             if current_value >= range.min && current_value <= range.max {
@@ -109,7 +188,7 @@ fn find_loc(seed: u64, maps: &HashMap<&str, HashMap<Range, i64>>) -> u64 {
     current_value
 }
 
-fn parse_maps(lines: &[String]) ->  HashMap<&str, SingleMap> {
+fn parse_maps(lines: &[String]) -> HashMap<&str, SingleMap> {
     let mut maps = HashMap::new();
     let mut header = None;
     for line in lines {
@@ -125,9 +204,17 @@ fn parse_maps(lines: &[String]) ->  HashMap<&str, SingleMap> {
             maps.insert(header.unwrap(), SingleMap::new());
         }
 
-        let nums = line.split(" ").map(|s| s.parse::<u64>().unwrap()).collect::<Vec<u64>>();
-        let range = Range { min: nums[1], max: nums[1] + nums[2] - 1 };
-        maps.get_mut(header.unwrap()).unwrap().insert(range, nums[0] as i64 - nums[1] as i64);
+        let nums = line
+            .split(" ")
+            .map(|s| s.parse::<u64>().unwrap())
+            .collect::<Vec<u64>>();
+        let range = Range {
+            min: nums[1],
+            max: nums[1] + nums[2] - 1,
+        };
+        maps.get_mut(header.unwrap())
+            .unwrap()
+            .insert(range, nums[0] as i64 - nums[1] as i64);
     }
 
     maps
