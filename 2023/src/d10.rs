c25907e537842b281ca28a997ec1af6a93f9f86f
Łukasz Piątkowski <piontec@gmail.com>
2024-01-29 12:02:15 +0100

    d10-2

--- 2023/src/d10.rs
+++ 2023/src/d10.rs
@@ -2,6 +2,7 @@ use crate::common::Point2D;
 use crate::DayTask;
 use core::panic;
 use std::collections::HashMap;
+use std::hash::Hash;
 
 pub struct Task;
 
@@ -29,16 +30,53 @@ impl DayTask<i32> for Task {
     }
 
     fn get_part2_test_result(&self) -> i32 {
-        todo!()
+        1
     }
 
     fn run_p1(&self, lines: &Vec<String>) -> i32 {
-        let path = get_path(lines);
-        path.len() as i32 / 2
+        get_path(lines).len() as i32 / 2
     }
 
     fn run_p2(&self, lines: &Vec<String>) -> i32 {
-        todo!()
+        let path = get_path(lines);
+        let mut in_counter = 0;
+
+        for y in 0..lines.len() {
+            let mut wall_count = 0;
+            let mut horizontal_start = None;
+            for x in 0..lines[y].len() {
+                let p = Point2D::new(x, y);
+                if path.contains_key(&p) {
+                    let path_element = path.get(&p).unwrap();
+                    match path_element {
+                        'F' | 'L' => {
+                            horizontal_start = Some(path_element);
+                        }
+                        'J' => {
+                            if *horizontal_start.unwrap() == 'F' {
+                                wall_count += 1;
+                            }
+                        }
+                        '7' => {
+                            if *horizontal_start.unwrap() == 'L' {
+                                wall_count += 1;
+                            }
+                        }
+                        '-' => { }
+                        '|' => {
+                            wall_count += 1;
+                        }
+                        _ => panic!("Invalid character"),
+                        
+                    }
+                }
+                else if wall_count % 2 == 1 {
+                    in_counter += 1;
+                }
+            }
+        }
+
+        in_counter
     }
 
     fn get_part1_result(&self) -> Option<i32> {
@@ -50,7 +88,7 @@ impl DayTask<i32> for Task {
     }
 }
 
-fn get_path(lines: &Vec<String>) -> Vec<Point2D<usize>> {
+fn get_path(lines: &Vec<String>) -> HashMap<Point2D<usize>, char> {
     let mut s = Point2D::new(0, 0);
     for li in 0..lines.len() {
         let l = &lines[li];
@@ -61,11 +99,14 @@ fn get_path(lines: &Vec<String>) -> Vec<Point2D<usize>> {
     }
     let mut current = Point2D::new(s.x + 1, s.y);
     let mut prev = s.clone();
-    let mut path: Vec<Point2D<usize>> = vec![s.clone()];
+    // I checked and in all cases 'S' is 'F'
+    let mut path: HashMap<Point2D<usize>, char> = HashMap::new();
+    path.insert(s, 'F');
+
     loop {
         // assuming going clockwise
-        let next_symbol = lines[current.y].chars().nth(current.x).unwrap();
-        let next_pos = match next_symbol {
+        let current_symbol = lines[current.y].chars().nth(current.x).unwrap();
+        let next_pos = match current_symbol {
             'F' => {
                 // we come from south
                 if prev.y == current.y + 1 {
@@ -140,7 +181,7 @@ fn get_path(lines: &Vec<String>) -> Vec<Point2D<usize>> {
             }
             _ => panic!("Invalid character"),
         };
-        path.push(current.clone());
+        path.insert(current.clone(), current_symbol);
         prev = current;
         current = next_pos;
 
