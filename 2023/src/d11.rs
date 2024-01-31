440acacfea7b8ec4700466604689972dffc71da9
Łukasz Piątkowski <piontec@gmail.com>
2024-01-31 15:13:16 +0100

    d11-2

--- 2023/src/d11.rs
+++ 2023/src/d11.rs
@@ -15,7 +15,7 @@ const TI: &str = "...#......
 .......#..
 #...#.....";
 
-impl DayTask<i32> for Task {
+impl DayTask<i64> for Task {
 
     fn day_no(&self) -> u8 {
         11
@@ -29,40 +29,59 @@ impl DayTask<i32> for Task {
         TI
     }
 
-    fn get_part1_test_result(&self) -> i32 {
+    fn get_part1_test_result(&self) -> i64 {
         374
     }
 
-    fn get_part2_test_result(&self) -> i32 {
-        todo!()
+    fn get_part2_test_result(&self) -> i64 {
+        82000210
     }
 
-    fn run_p1(&self, lines: &Vec<String>) -> i32 {
-        let expanded = expand(lines);
-        let star_map = get_star_map(&expanded);
-
-        let mut total_dist = 0;
-
-        for source_index in 0..(star_map.len() - 1) {
-            for dest_index in (source_index + 1)..star_map.len() {
-                total_dist += star_map[source_index].manhattan_distance(&star_map[dest_index]);
-            }
-        }
-
-        total_dist as i32
+    fn run_p1(&self, lines: &Vec<String>) -> i64 {
+        run(lines, 2)
     }
 
-    fn run_p2(&self, lines: &Vec<String>) -> i32 {
-        todo!()
+    fn run_p2(&self, lines: &Vec<String>) -> i64 {
+        run(lines, 1000000)
     }
 
-    fn get_part1_result(&self) -> Option<i32> {
+    fn get_part1_result(&self) -> Option<i64> {
         Some(9509330)
     }
 
-    fn get_part2_result(&self) -> Option<i32> {
+    fn get_part2_result(&self) -> Option<i64> {
         None
     }
+}
+
+fn run(lines: &Vec<String>, factor: isize) -> i64 {
+    let star_map = get_star_map(lines);
+    let (rows, cols) = get_empty(lines);
+    let expanded_star_map = expand(&star_map, rows, cols, factor);
+
+    let mut total_dist = 0;
+
+    for source_index in 0..(expanded_star_map.len() - 1) {
+        for dest_index in (source_index + 1)..expanded_star_map.len() {
+            total_dist += expanded_star_map[source_index].manhattan_distance(&expanded_star_map[dest_index]);
+        }
+    }
+
+    total_dist as i64
+}
+
+fn expand(star_map: &[Point2D<isize>], 
+    rows: Vec<usize>, 
+    cols: Vec<usize>,
+    factor: isize) -> Vec<Point2D<isize>> {
+    let mut expanded = Vec::new();
+    for p in star_map {
+        let new_x = cols.iter().filter(|c| (**c as isize) < p.x).count() as isize * (factor - 1) + p.x;
+        let new_y = rows.iter().filter(|c| (**c as isize) < p.y).count() as isize * (factor - 1) + p.y;
+        expanded.push(Point2D::new(new_x, new_y));
+    }
+
+    expanded
 }
 
 fn get_star_map(expanded: &Vec<String>) -> Vec<Point2D<isize>> {
@@ -78,33 +97,28 @@ fn get_star_map(expanded: &Vec<String>) -> Vec<Point2D<isize>> {
     star_map
 }
 
-fn expand(lines: &Vec<String>) -> Vec<String> {
-    let mut expanded = Vec::new();
+fn get_empty(lines: &Vec<String>) -> (Vec<usize>, Vec<usize>) {
+    let rows = lines
+        .iter()
+        .enumerate()
+        .filter(|(_, line)| line.chars().all(|c| c == '.'))
+        .map(|(i, _)| i)
+        .collect();
 
-    // expand rows
-    for line in lines {
-        expanded.push(line.clone());
-        if line.chars().all(|c| c == '.') {
-            expanded.push(line.clone());
-        }
-    }
-
+    let mut cols = Vec::new();
     // expand columns
-    for ci in (0..expanded[0].len()).rev() {
+    for ci in (0..lines[0].len()).rev() {
         let mut all_empty = true;
-        for line in &expanded {
+        for line in lines {
             if line.as_bytes()[ci] == b'#' {
                 all_empty = false;
                 break;
             }
         }
-        if !all_empty {
-            continue;
-        }
-        for line in &mut expanded {
-            line.insert(ci, '.');
+        if all_empty {
+            cols.push(ci);
         }
     } 
 
-    expanded
+    (rows, cols)
 }
