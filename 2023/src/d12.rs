4ca6db0a56d0e159ac70cb06fc54fea3c4a4d10b
Łukasz Piątkowski <piontec@gmail.com>
2024-07-03 11:38:29 +0200

    d12-1

--- 2023/src/d12.rs
+++ 2023/src/d12.rs
@@ -3,6 +3,12 @@ use std::collections::HashMap;
 
 pub struct Task;
 
+enum Result {
+    Valid,
+    Invalid,
+    Maybe(u8)
+}
+
 const TI: &str = "???.### 1,1,3
 .??..??...?##. 1,1,3
 ?#?#?#?#?#?#?#? 1,3,1,6
@@ -11,13 +17,12 @@ const TI: &str = "???.### 1,1,3
 ?###???????? 3,2,1";
 
 impl DayTask<i64> for Task {
-
     fn day_no(&self) -> u8 {
         12
     }
 
     fn get_part1_test_input(&self) -> &'static str {
-        21
+        TI
     }
 
     fn get_part2_test_input(&self) -> &'static str {
@@ -25,7 +30,7 @@ impl DayTask<i64> for Task {
     }
 
     fn get_part1_test_result(&self) -> i64 {
-        todo!()
+        21
     }
 
     fn get_part2_test_result(&self) -> i64 {
@@ -33,7 +38,7 @@ impl DayTask<i64> for Task {
     }
 
     fn run_p1(&self, lines: &Vec<String>) -> i64 {
-        todo!()
+        lines.iter().map(|line| count_permutations(line)).sum()
     }
 
     fn run_p2(&self, lines: &Vec<String>) -> i64 {
@@ -47,4 +52,72 @@ impl DayTask<i64> for Task {
     fn get_part2_result(&self) -> Option<i64> {
         None
     }
+}
+
+fn count_permutations(line: &String) -> i64 {
+    let (chars_str, counts_str) = line
+        .split_once(" ")
+        .unwrap();
+    let expected_counts: Vec<i32> = counts_str
+        .split(",")
+        .map(|s| s.parse().unwrap())
+        .collect();
+    let chars: String = String::from(chars_str);
+
+    let mut combinations = 0;
+    let mut to_check = Vec::new();
+    to_check.push((chars, &expected_counts));
+
+    while to_check.len() > 0 {
+        let (to_check_chars, to_check_counts) = to_check.pop().unwrap();
+
+        match is_valid(&to_check_chars, &to_check_counts) {
+            Result::Valid => {
+                combinations += 1;
+            },
+            Result::Invalid => {
+                continue;
+            },
+            Result::Maybe(ind) => {
+                let has = to_check_chars[..ind as usize].to_string() + "#" + &to_check_chars[(ind as usize + 1)..];
+                let has_not = to_check_chars[..ind as usize].to_string() + "." + &to_check_chars[(ind as usize + 1)..];
+                to_check.push((has, to_check_counts));
+                to_check.push((has_not, to_check_counts));
+            } 
+        }
+    }
+    combinations
+}
+
+fn is_valid(bytes_str: &str, expected_counts: &Vec<i32>) -> Result {
+    let mut in_stream_block = false;
+    let mut block_counts = Vec::new();
+    let mut block_len = 0;
+    for (i, b) in bytes_str.chars().enumerate() {
+        if b == '#' {
+            if !in_stream_block {
+                in_stream_block = true;
+            }
+            block_len += 1;
+            if i == bytes_str.len() - 1 {
+                block_counts.push(block_len);
+            }
+        } else if b == '.' {
+            if in_stream_block {
+                if expected_counts.len() == block_counts.len() {
+                    return Result::Invalid;
+                }
+                if expected_counts[block_counts.len()] != block_len {
+                    return Result::Invalid;
+                }
+
+                block_counts.push(block_len);
+                block_len = 0;
+                in_stream_block = false;
+            }
+        } else {
+            return Result::Maybe(i as u8);
+        }
+    }
+    if block_counts == *expected_counts { Result::Valid } else { Result::Invalid }
 }
