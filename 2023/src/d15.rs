5fef9e6feccc505feed4b3aad8df79f29654f419
Łukasz Piątkowski <piontec@gmail.com>
2024-10-03 14:14:38 +0200

    d15-2

--- 2023/src/d15.rs
+++ 2023/src/d15.rs
@@ -1,9 +1,19 @@
 use crate::DayTask;
-use std::collections::HashMap;
 
 pub struct Task;
 
 const TI: &str = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
+
+#[derive(Debug, Clone)]
+struct Lens {
+    label: String,
+    power: u8,
+}
+
+enum Op {
+    Remove,
+    Add,
+}
 
 impl DayTask<i64> for Task {
 
@@ -24,7 +34,7 @@ impl DayTask<i64> for Task {
     }
 
     fn get_part2_test_result(&self) -> i64 {
-        todo!()
+        145
     }
 
     fn run_p1(&self, lines: &Vec<String>) -> i64 {
@@ -36,15 +46,56 @@ impl DayTask<i64> for Task {
     }
 
     fn run_p2(&self, lines: &Vec<String>) -> i64 {
-        todo!()
+        let instructions: Vec<&str> = lines[0].split(",").collect();
+        let mut boxes = vec![Vec::<Lens>::new(); 256];
+
+        for inst in instructions {
+            let parts = inst.split(['=', '-']).collect::<Vec<&str>>();
+            let label = parts[0];
+            let op = if parts[1] == "" { Op::Remove } else { Op::Add };
+            let box_index = get_hash(label);
+            match op {
+                Op::Add => {
+                    if let Some(pos) = boxes[box_index].iter().position(|l| l.label == label) {
+                        boxes[box_index][pos].power = parts[1].parse::<u8>().unwrap();
+                    }
+                    else {
+                        boxes[box_index].push(
+                            Lens {
+                                label: label.to_string(),
+                                power: parts[1].parse::<u8>().unwrap(),
+                            }
+                        );
+                    }
+                }
+                Op::Remove => {
+                    if let Some(pos) = boxes[box_index].iter().position(|l| l.label == label) {
+                        boxes[box_index].remove(pos);
+                    }
+                }
+            }
+        }
+        let focus_pow = boxes
+            .iter()
+            .enumerate()
+            .map(|(box_index, b)| {
+                let box_pow = b
+                .iter()
+                .enumerate()
+                .map(|(lens_index, l)| (box_index + 1)*(lens_index +1)*l.power as usize)
+                .sum::<usize>();
+                box_pow
+            })
+            .sum::<usize>();
+        focus_pow as i64
     }
 
     fn get_part1_result(&self) -> Option<i64> {
-        None
+        Some(515495)
     }
 
     fn get_part2_result(&self) -> Option<i64> {
-        None
+        Some(229349)
     }
 }
 
