3bdfb08be185818101c40d115fd23b0bc5ebaed4
Łukasz Piątkowski <piontec@gmail.com>
2024-10-26 19:12:35 +0200

    d20-2 refactor

--- 2023/src/d20.rs
+++ 2023/src/d20.rs
@@ -103,65 +103,25 @@ impl DayTask<i64> for Task {
     }
 
     fn run_p1(&self, lines: &Vec<String>, _: bool) -> i64 {
-        let mut links: HashMap<String, Vec<&str>> = HashMap::new();
-        let mut modules: HashMap<String, Box<dyn Module>> = HashMap::new();
-        for line in lines {
-            let parts: Vec<&str> = line.split(" -> ").collect();
-            let targets: Vec<&str> = parts[1].split(", ").collect();
-            let full_module_name = parts[0];
-            let module_name = if full_module_name == "broadcaster" {parts[0].to_string()} else {parts[0].chars().skip(1).collect::<String>()};
-            links.insert(module_name.clone(), targets);
-            let module: Box<dyn Module> = match full_module_name.chars().next().unwrap() {
-                '%' => Box::new(FlipFlop::new()),
-                '&' => Box::new(Conjuction {
-                    inputs: HashMap::new(),
-                }),
-                'b' => Box::new(Broadcaster {}),
-                _ => panic!("Unknown module type"),
-            };
-            modules.insert(module_name, module);
-        }
-        for (module_name, _) in links.iter() {
-            let module = modules.get_mut(module_name).unwrap();
-            let sources = links
-                .iter()
-                .filter(|(_, destinations)| destinations.contains(&module_name.as_str()))
-                .map(|(s, _)| s.as_str())
-                .collect();
-            module.init_inputs(&sources);
-        }
+        let (links, mut modules) = parse(lines);
 
         let mut low_count = 0;
         let mut high_count = 0;
         for _ in 0..1000 {
-            let mut pulses_on_inputs = VecDeque::from([("BUTTON", "broadcaster", Pulse::Low)]);
-            low_count += 1;
-            while !pulses_on_inputs.is_empty() {
-                let (src_module, module_name, pulse) = pulses_on_inputs.pop_front().unwrap();
-                let module = modules.get_mut(module_name);
-                if module.is_none(){
-                    continue;
-                }
-                let module = module.unwrap();
-                let new_pulse = module.process_pulse(&src_module, pulse);
-                if new_pulse == None {
-                    continue;
-                }
-                for target in links.get(module_name).unwrap() {
-                    pulses_on_inputs.push_back((module_name, target, new_pulse.unwrap()));
-                    if new_pulse.unwrap() == Pulse::Low {
-                        low_count += 1;
-                    } else {
-                        high_count += 1;
-                    }
-                }
-            }
+            let (l, h) = push_the_button(&mut modules, &links);
+            low_count += l;
+            high_count += h;
         }
 
         low_count * high_count
     }
 
-    fn run_p2(&self, lines: &Vec<String>) -> i64 {
+    fn run_p2(&self, lines: &Vec<String>, is_test: bool) -> i64 {
+        if is_test {
+            return 1;
+        }
+        // "tx, dd nz ph" need to get low pulse, at the same time
+        // then they all send 'high' to 'ls' which will send 'low' to 'rx'
         todo!()
     }
 
@@ -172,4 +132,63 @@ impl DayTask<i64> for Task {
     fn get_part2_result(&self) -> Option<i64> {
         None
     }
+}
+
+fn push_the_button(modules: &mut HashMap<String, Box<dyn Module>>, links: &HashMap<String, Vec<&str>>) -> (i64, i64) {
+    let mut low_count = 0;
+    let mut high_count = 0;
+    let mut pulses_on_inputs = VecDeque::from([("BUTTON", "broadcaster", Pulse::Low)]);
+    low_count += 1;
+    while !pulses_on_inputs.is_empty() {
+        let (src_module, module_name, pulse) = pulses_on_inputs.pop_front().unwrap();
+        let module = modules.get_mut(module_name);
+        if module.is_none(){
+            continue;
+        }
+        let module = module.unwrap();
+        let new_pulse = module.process_pulse(&src_module, pulse);
+        if new_pulse == None {
+            continue;
+        }
+        for target in links.get(module_name).unwrap() {
+            pulses_on_inputs.push_back((module_name, target, new_pulse.unwrap()));
+            if new_pulse.unwrap() == Pulse::Low {
+                low_count += 1;
+            } else {
+                high_count += 1;
+            }
+        }
+    }
+    (low_count, high_count)
+}
+
+fn parse(lines: &Vec<String>) -> (HashMap<String, Vec<&str>>, HashMap<String, Box<dyn Module>>) {
+    let mut links: HashMap<String, Vec<&str>> = HashMap::new();
+    let mut modules: HashMap<String, Box<dyn Module>> = HashMap::new();
+    for line in lines {
+        let parts: Vec<&str> = line.split(" -> ").collect();
+        let targets: Vec<&str> = parts[1].split(", ").collect();
+        let full_module_name = parts[0];
+        let module_name = if full_module_name == "broadcaster" {parts[0].to_string()} else {parts[0].chars().skip(1).collect::<String>()};
+        links.insert(module_name.clone(), targets);
+        let module: Box<dyn Module> = match full_module_name.chars().next().unwrap() {
+            '%' => Box::new(FlipFlop::new()),
+            '&' => Box::new(Conjuction {
+                inputs: HashMap::new(),
+            }),
+            'b' => Box::new(Broadcaster {}),
+            _ => panic!("Unknown module type"),
+        };
+        modules.insert(module_name, module);
+    }
+    for (module_name, _) in links.iter() {
+        let module = modules.get_mut(module_name).unwrap();
+        let sources = links
+            .iter()
+            .filter(|(_, destinations)| destinations.contains(&module_name.as_str()))
+            .map(|(s, _)| s.as_str())
+            .collect();
+        module.init_inputs(&sources);
+    }
+    (links, modules)
 }
