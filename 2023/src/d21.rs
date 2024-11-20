a331f0dfd5b27384ed3d17a9577f870ba1c7a89a
Łukasz Piątkowski <piontec@gmail.com>
2024-11-20 16:29:56 +0100

    d21-2

--- 2023/src/d21.rs
+++ 2023/src/d21.rs
@@ -39,7 +39,7 @@ impl DayTask<i64> for Task {
     }
 
     fn get_part2_test_result(&self) -> i64 {
-        todo!()
+        -1
     }
 
     fn run_p1(&self, lines: &Vec<String>, is_test: bool) -> i64 {
@@ -55,8 +55,46 @@ impl DayTask<i64> for Task {
         gardens as i64
     }
 
-    fn run_p2(&self, lines: &Vec<String>, _: bool) -> i64 {
-        todo!()
+    fn run_p2(&self, lines: &Vec<String>, is_test: bool) -> i64 {
+        if is_test {
+            return -1;
+        }
+        // totally based on https://advent-of-code.xavd.id/writeups/2023/day/21/
+        let map = MapHashMap::parse_map(lines);
+        let start = map.find('S')[0];
+        let shortest_paths = bfs_to_all(&map, *start);
+
+        let grid_size = lines.len();
+        let distance_to_edge = grid_size / 2;
+        assert!(distance_to_edge == 65);
+        let n = (26501365 - distance_to_edge) / grid_size;
+        assert!(n == 202300);
+        let num_odd_tiles = (n + 1) * (n + 1);
+        let num_even_tiles = n * n;
+
+        let odd_corners = shortest_paths
+            .map
+            .iter()
+            .filter(|(_, &s)| s > distance_to_edge as i32 && s % 2 == 1)
+            .count();
+        let even_corners = shortest_paths
+            .map
+            .iter()
+            .filter(|(_, &s)| s > distance_to_edge as i32 && s % 2 == 0)
+            .count();
+        let all_odd = shortest_paths
+            .map
+            .iter()
+            .filter(|(_, &s)| s % 2 == 1)
+            .count();
+        let all_even = shortest_paths
+            .map
+            .iter()
+            .filter(|(_, &s)| s % 2 == 0)
+            .count();
+        let res = num_odd_tiles * all_odd + num_even_tiles * all_even - ((n + 1) * odd_corners)
+            + (n * even_corners);
+        res as i64
     }
 
     fn get_part1_result(&self) -> Option<i64> {
@@ -64,7 +102,7 @@ impl DayTask<i64> for Task {
     }
 
     fn get_part2_result(&self) -> Option<i64> {
-        None
+        Some(623540829615589)
     }
 }
 
